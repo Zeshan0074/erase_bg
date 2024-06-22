d90c5200f46a36a273a70ff8b09324bfdd685dae
Gulfam Haider <gulfam.cortechsols@gmail.com>
2024-06-22 10:54:26 +0500

    Errors of height is resolved and paypal create subscription is working perfectly

--- src/store/actions/paypalAction.js
+++ src/store/actions/paypalAction.js
@@ -1,17 +1,24 @@
 import toast from "react-hot-toast";
 import { RepositoryFactory } from "../../repository/RepositoryFactory";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 var paypal = RepositoryFactory.get("paypal");
-
+const history = useHistory
 // // This action is for Signup
 export const paypalprocessing = (payload,onSuccess) => async (dispatch) => {
     await dispatch(loginLoading(true));
 
     try {
-        const data = await paypal.paypalprocessing(payload);
-        console.log("What is the this Paypal ",data)
-        if (data.status === 200 ) {
+        const {data} = await paypal.paypalprocessing(payload);
+        
+        if (data?.status == "APPROVAL_PENDING" ) {
             dispatch(loginLoading(false));
-            onSuccess();
+            // onSuccess();
+            const redirectLink = data?.links[0]?.href;
+            console.log("here",redirectLink);
+            if (redirectLink) {
+                console.log("What is the this Paypal ",redirectLink)
+                window.location.href = redirectLink;  // Redirect to PayPal link
+            }
         }
     } catch (error) {
         console.error("Sorry you missing or mismatch some fileds");
